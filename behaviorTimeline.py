17bece78f20330881232bb458efef670fe76410b
Sam Gale <samueldgale@gmail.com>
2018-10-08 18:29:49 -0700

    etc

--- behaviorTimeline.py
+++ behaviorTimeline.py
@@ -23,8 +23,17 @@ ephysDates = ('09072018','09102018','09112018','09172018')
 mouseID = '390339'
 ephysDates = ('09192018','09202018','09212018')
 
-ephysDates = [datetime.datetime.strptime(d,'%m%d%Y') for d in ephysDates]
+mouseID = '394873'
+ephysDates = ('10042018','10052018')
 
+mouseID = '404788'
+ephysDates = None
+
+mouseID = '403472'
+ephysDates = None
+
+
+ephysDates = [datetime.datetime.strptime(d,'%m%d%Y') for d in ephysDates] if ephysDates is not None else (None,)
 frameRate = 60
 window = 300*frameRate
 interval = 60*frameRate
@@ -79,15 +88,19 @@ t = np.array([(d-min(trainingDate)).days+1 for d in trainingDate])
 isEphys = np.array([d in ephysDates for d in trainingDate])
 
 fig = plt.figure(facecolor='w',figsize=(8,10))
-for i,(prm,lbl) in enumerate(zip((rewardsEarned,lastEngaged,probEngaged),('Rewards Earned','Last Engaged (s)','Probability Engaged'))):
+for i,(prm,ymax,lbl) in enumerate(zip((rewardsEarned,lastEngaged,probEngaged),(None,3660,1),('Rewards Earned','Last Engaged (s)','Probability Engaged'))):
     ax = plt.subplot(3,1,i+1)
     for ind,clr in zip((~isEphys,isEphys),'kr'):
         ax.plot(t[ind],np.array(prm)[ind],'o',mec=clr,mfc=clr,ms=8)
     for side in ('right','top'):
         ax.spines[side].set_visible(False)
     ax.tick_params(direction='out',top=False,right=False,labelsize=12)
-    ax.set_ylim([0,1.05*max(prm)])
+    ax.set_xlim([0,max(t)+1])
+    ylimMax = max(prm) if ymax is None else ymax
+    ax.set_ylim([0,1.05*ylimMax])
     ax.set_ylabel(lbl,fontsize=14)
+    if i==0:
+        ax.set_title(mouseID,fontsize=14)
     if i==2:
         ax.set_xlabel('Training Day',fontsize=14)
 plt.tight_layout()
