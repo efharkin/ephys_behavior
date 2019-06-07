84f435e4cdb716b272497ddb3cddbadaedae82d8
Corbett Bennett <corbettb@alleninstitute.org>
2019-06-07 14:11:34 -0700

    fixed bug in warpImage

--- warpImage.py
+++ warpImage.py
@@ -10,7 +10,7 @@ import cv2
 
 def warpImage(refImg,warpImg,refPts,warpPts):
     # append boundaryPts to npts x 2 (x,y) float32 point arrays
-    refPts,warpPts = (np.concatenate((pts,getBoundaryPoints(shape[1],shape[0])),axis=0).astype(np.float32) for pts,shape in zip((refPts,warpPts),(refImg,warpImg)))
+    refPts,warpPts = (np.concatenate((pts,getBoundaryPoints(shape[1],shape[0])),axis=0).astype(np.float32) for pts,shape in zip((refPts,warpPts),(refImg.shape,warpImg.shape)))
     
     # get Delaunay triangles as indices of refPts (point1Index,point2Index,point3Index)
     triangles = getDelauneyTriangles(refPts,refImg.shape[1],refImg.shape[0])
@@ -33,7 +33,7 @@ def warpImage(refImg,warpImg,refPts,warpPts):
         cv2.fillConvexPoly(mask,refTri.astype(int),1)
         mask = mask.astype(bool)
         warpMatrix = cv2.getAffineTransform(warpTri,refTri)
-        for ch in range(newImg.shape[3]):
+        for ch in range(newImg.shape[2]):
             warpData = cv2.warpAffine(warpImg[warpSlice[0],warpSlice[1],ch],warpMatrix,refRect[2:],flags=cv2.INTER_LINEAR,borderMode=cv2.BORDER_REFLECT_101)
             newImg[refSlice[0],refSlice[1],ch][mask] = warpData[mask]
     return newImg
