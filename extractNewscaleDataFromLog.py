5d032ba8c6aad5465139d8c27422ed62db5c141e
Corbett Bennett <corbettb@alleninstitute.org>
2019-07-25 18:49:46 -0700

    Update extractNewscaleDataFromLog.py

--- extractNewscaleDataFromLog.py
+++ extractNewscaleDataFromLog.py
@@ -15,12 +15,34 @@ df = pd.read_csv(logFile, header=None, names=['time', 'probeID', 'x', 'y', 'z', 
 df['time'] = pd.to_datetime(df['time'])
 df = df.set_index('time')
 
-probe = ' SN31058'
+pid = ' SN34029'
 dateOfInterest = '2019-04-25'
-pdf = df.loc[(df.probeID==probe)]
-pdf = pdf.loc[dateOfInterest]
+pdf = df.loc[dateOfInterest]
 
-timeDeltas = pdf.index.to_series().diff().dt.microseconds
-timeDeltas[0] = 0
-
-plt.plot(timeDeltas, pdf['z'])
+for pid in np.unique(pdf.probeID.values):
+    tempdf = pdf.loc[pdf.probeID==pid]
+    tempdf.plot(y='z')
+    diffs = tempdf['z'].diff()
+    timeDeltas = tempdf.index.to_series().diff().astype('timedelta64[s]')
+    
+    #find the first time such that the next 20 deltas are all small
+    rollingDelta = timeDeltas.rolling(10, win_type='boxcar').sum().dropna()
+    start = rollingDelta.where(rollingDelta<1).dropna().index[0]
+    end = rollingDelta.where(rollingDelta<1).dropna().index[-1]
+    
+    #insertionTimes = timeDeltas.where((timeDeltas<0.1)&(timeDeltas.shift(periods=-1)<0.1).dropna(axis=0)
+    #insertionTimes = timeDeltas.where((timeDeltas<0.1)&(timeDeltas.shift(periods=-10)<0.1)&(timeDeltas.shift(periods=-20)<0.1)).dropna(axis=0)
+    #insertionTimes = timeDeltas.where((timeDeltas<0.1)&(abs(diffs))<10).dropna(axis=0)
+    
+    
+    insertiondf = tempdf.loc[start:end]
+    
+    ax = plt.gca()
+    ax.set_title(pid)
+    ax.plot(start, insertiondf.iloc[0, 3], 'go')
+    ax.plot(end, insertiondf.iloc[-1, 3], 'ro')
+    
+    print(pid)
+    print('Insertion start coords: ' + str(insertiondf.iloc[0, 0:4]))
+    print('Insertion end coords: ' + str(insertiondf.iloc[-1, 0:4]))
+    print('\n\n')
